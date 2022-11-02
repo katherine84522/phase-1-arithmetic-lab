54bf46b6c4aa86ee0c11c155bfed1e0fc823df80
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-11-01 21:55:13 -0400

    commit tree 962e53d4b5f0ed634a36257c6803ea34d8847239

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 19;
+let num4 = 5;
+const mod = num3 % num4;
+
+let num5 = 20;
+const max = Math.max(num2, num4, num5);
+
+
+
+
